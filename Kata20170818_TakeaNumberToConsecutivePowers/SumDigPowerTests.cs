33a0e2616d2a5dd3857e6c038eb27ae34bad0bfe
cash <cash@cashwu.com>
2017-08-18 17:32:09 +0800

    input 1, 10 test case

---
+++ Kata20170818_TakeaNumberToConsecutivePowers/SumDigPowerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170818_TakeaNumberToConsecutivePowers
+{
+    [TestClass]
+    public class SumDigPowerTests
+    {
+        [TestMethod]
+        public void input_1_10()
+        {
+            SumDigPowerShouldBe(new long[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, 1, 10);
+        }
+
+        private static void SumDigPowerShouldBe(long[] expected, int a, int b)
+        {
+            var actual = new SumDigPower().SumDigPow(a, b);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class SumDigPower
+    {
+        public long[] SumDigPow(long a, long b)
+        {
+            return new long[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        }
+    }
+}
